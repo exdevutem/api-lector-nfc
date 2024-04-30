bbc859dda277b47ba005c11741f48df069cfa8c4
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-04-30 13:29:38 -0400

    Commit Inicial
    Signed-off-by: Francisco Solis <30329003+Im-Fran@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+## API Lector NFC
+Esta es una api simple que permite revisar si un tag NFC es válido y es miembro del club.
+
+## Instalación
+Para instalar la api, se debe clonar el repositorio y ejecutar los siguientes comandos:
+```
+pnpm install
+pnpm run dev
+```
+
+## Contribuidores
+Este proyecto fue creado por el Club de Desarrollo Experimental (ExDev) de la Universidad Tecnológica Metropolitana y es mantenido por los propios estudiantes. Mira los perfiles que han contribuido a este proyecto:
+
+<a href="https://github.com/exdevutem/api-lector-nfc/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=exdevutem/api-lector-nfc" alt="Contribuidores" />
+</a>
---
+++ src/index.ts
@@ -0,0 +1,74 @@
+import { Hono } from 'hono'
+import {Client} from "@notionhq/client";
+import {env} from "hono/adapter";
+
+const app = new Hono()
+
+/* Válida el NFC entregado */
+app.get('/:nfc_id', async (c) => {
+  const nfc_id = c.req.param('nfc_id')
+  const { NOTION_API_KEY } = env<{ NOTION_API_KEY: string }>(c)
+  const notion = new Client({ auth: NOTION_API_KEY });
+  const databaseResponse = await notion.databases.query({
+    database_id: 'a99a85ba5ca44834bd747cac820bb6e0',
+    filter: {
+      and: [
+        {
+          property: 'NFC ID',
+          rich_text: {
+            contains: nfc_id
+          }
+        },
+        {
+          property: 'Puesto de trabajo',
+          select: {
+            equals: 'Miembro'
+          }
+        },
+        {
+          property: 'Organización',
+          multi_select: {
+            contains: '✨ ExDev ✨'
+          }
+        }
+      ]
+    }
+  });
+
+  if(databaseResponse.results.length === 0) {
+    return c.json({
+      error: {
+        mensaje: `No encontramos el tag ${nfc_id}!`,
+        codigo: 1
+      }
+    }, 404)
+  }
+
+  const idPagina = databaseResponse.results[0].id
+
+  const pageResponse: any = await notion.pages.retrieve({
+    page_id: idPagina
+  });
+
+  const properties = pageResponse['properties']
+
+  if(!properties) {
+    return c.json({
+      error: {
+        mensaje: `No encontramos propiedades en la página!`,
+        codigo: 2
+      }
+    }, 404)
+  }
+
+  const puesto = properties['Puesto de trabajo']['select']['name']
+  const nombre = properties['Nombre']['title'][0]['plain_text']
+
+  return c.json({
+    nombre,
+    puesto,
+    nfc_id,
+  })
+})
+
+export default app
